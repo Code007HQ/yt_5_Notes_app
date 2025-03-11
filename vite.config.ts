9e012246f9cdca7b92e3c2ea11bd9e58b4cc5492
Code007HQ <code007.dev@gmail.com>
2025-03-11 11:33:00 +0530

    Fix path issue
    Fix url issue
    
    Fix type issue
    
    Fix path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import path from "path";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import path from "path-browserify";
+import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+// Convert import.meta.url to a file path (ES module compatible)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
