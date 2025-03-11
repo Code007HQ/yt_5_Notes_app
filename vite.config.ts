f535a5f467ffb8375fe96615c5fd6490dd2374d9
Code007HQ <code007.dev@gmail.com>
2025-03-11 11:33:00 +0530

    Fix path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import path from "path";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import path from "path-browserify"; // ✅ Use path-browserify instead
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.url, "./src"), // ✅ Fix __dirname issue
     },
   },
 });
